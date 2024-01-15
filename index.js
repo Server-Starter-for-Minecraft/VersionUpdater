c7a58fd9b5db6e063fac5fdb7718b204c684687a
txkodo <kodo.hsmkai@gmail.com>
2024-01-15 23:18:27 +0900

    ✨ created gitgub action

---
+++ index.js
@@ -0,0 +1,43 @@
+const core = require('@actions/core');
+const fs = require('fs');
+
+try {
+    const path = core.getInput('json-path',{trimWhitespace:true}) ?? "package.json";
+    const content = JSON.parse(fs.readFileSync(path,{encoding:"utf8"}))
+    
+    let [major, minor, patch] = content.version.split('.').map(x => parseInt(x));
+
+    let release_type = core.getInput('release-type',{trimWhitespace:true})
+
+    core.setOutput("current-version",`${major}.${minor}.${patch}`)
+
+    switch (release_type){
+        case "major":
+            major += 1
+            minor = 0
+            patch = 0
+            break;
+        case "minor":
+            minor += 1
+            patch = 0
+            break;
+        case "patch":
+        default:
+            patch += 1
+            break;
+    }
+
+    const next_version = `${major}.${minor}.${patch}`
+    core.setOutput("next-version",next_version)
+
+    const updateJsonFile = core.getInput('update-json',{trimWhitespace:true}).toLowerCase() === "true";
+
+    if (updateJsonFile){
+        content.version = next_version
+        fs.writeFileSync('package.json', JSON.stringify(content, undefined, 2), {
+            encoding: 'utf8',
+            });
+    }
+} catch (error) {
+  core.setFailed(error.message);
+}
